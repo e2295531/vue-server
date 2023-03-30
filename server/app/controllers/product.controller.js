cb81a18f9e8ac0c969c8d7a8278f3de872d7fcee
e2295531 <sfennoun@gmail.com>
2023-03-30 16:27:32 -0400

    ajout update et delete

--- server/app/controllers/product.controller.js
+++ server/app/controllers/product.controller.js
@@ -31,3 +31,57 @@ exports.myCreate=(req,res)=>{
         })
     })
 }
+exports.myFindOne=(req,res)=>{
+    const id=req.params.id
+    Product.findByPk(id)
+    .then(data => {
+        res.send(data)
+    })
+    .catch(err =>{
+        res.status(500).send({
+            message:'Could not insert the data'
+        })
+    })
+}
+exports.myDestroy=(req,res)=>{
+    const id=req.params.id
+    Product.destroy({where:{id: id}})
+    .then(num => {
+       if(num==1){
+        res.send({
+            message:'product was deleted'
+        })
+       }
+       else{
+        res.send({
+            message:'product was not deleted'
+        })
+       }
+    })
+    .catch(err =>{
+        res.status(500).send({
+            message:'Could not insert the data'
+        })
+    })
+}
+exports.myUpdate=(req,res)=>{
+    const id=req.params.id
+    Product.update(req.body,{where:{id: id}})
+    .then(num => {
+       if(num==1){
+        res.send({
+            message:'product was updated'
+        })
+       }
+       else{
+        res.send({
+            message:'product was not updated'
+        })
+       }
+    })
+    .catch(err =>{
+        res.status(500).send({
+            message:'Could not insert the data'
+        })
+    })
+}
